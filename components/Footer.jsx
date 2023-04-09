3f6d7b90459d964af82b886681db6f260c09a336
bagabriel367 <bagabriel@outlookc.com.br>
2023-04-09 12:01:18 -0300

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2023 North America Products ®Todos os direitos reservados</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  )
+}
+
+export default Footer
